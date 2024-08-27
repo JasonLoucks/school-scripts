f0a9599390d7be713265fbc338ea7f5d66a45eb0
Jason Loucks <118084663+JasonLoucks@users.noreply.github.com>
2024-08-27 10:12:40 -0700

    Update CSCS.bat
    Updated and confirmed to work on school computers.

--- batch/CSCS.bat
+++ batch/CSCS.bat
@@ -1,13 +1,23 @@
 @echo off
-:: remove certs without prompt https://superuser.com/questions/191038/installing-deleting-root-certificate-without-certmgr-certutil-asking-the-end-u
 
-:: self-signing (following line) should be unnecessary? "-ExecutionPolicy Bypass" seems to work
-:: Powershell "$user = 'Jason Loucks'; Get-ChildItem Cert:\CurrentUser\ -Recurse | Where-Object {$_.Subject -match $user} | Remove-Item; $authenticode = New-SelfSignedCertificate -Subject 'Jason Loucks' -CertStoreLocation Cert:\CurrentUser\My -Type CodeSigningCert; $rootStore = [System.Security.Cryptography.X509Certificates.X509Store]::new('Root','CurrentUser'); $rootStore.Open('ReadWrite'); $rootStore.Add($authenticode); $rootStore.Close(); $publisherStore = [System.Security.Cryptography.X509Certificates.X509Store]::new('TrustedPublisher','CurrentUser'); $publisherStore.Open('ReadWrite'); $publisherStore.Add($authenticode); $publisherStore.Close(); Get-ChildItem Cert:\CurrentUser\My | Where-Object {$_.Subject -eq 'CN=$user'}; Get-ChildItem Cert:\CurrentUser\Root | Where-Object {$_.Subject -eq 'CN=$user'}; Get-ChildItem Cert:\CurrentUser\TrustedPublisher | Where-Object {$_.Subject -eq 'CN=$user'}; $codeCertificate = Get-ChildItem Cert:\CurrentUser\My | Where-Object {$_.Subject -eq 'CN=Jason Loucks'}; Set-AuthenticodeSignature -FilePath Documents\Scripting\ChangeSchoolComputerSettings.ps1 -Certificate $codeCertificate -TimeStampServer http://timestamp.digicert.com"
+:: Save the launcher script's working directory
+:: Change the working directory to this script's
+:: - This is so we can use relative paths when running the ps1
+set origDir=%CD%
+cd /d "%~dp0"
 
-set CSCS_ps1=..\powershell\test.ps1
+:: Location of the CSCS ps1 script
+set CSCS_ps1=..\powershell\CSCS.ps1
 
-set SUFTA=
+powershell -ExecutionPolicy Bypass %CSCS_ps1%
 
-Powershell %CSCS_ps1%
-PortableApps\SetUserFTA\SetUserFTA PortableApps\SetUserFTA\config.txt
+:: Go back to the original working directory so we can run SetUserFTA
+cd %origDir%
+
+:: Locations of the SUFTA folder and files
+set SUFTA=%~d0\PortableApps\SetUserFTA
+set SUFTA_exe=%SUFTA%\SetUserFTA.exe
+set SUFTA_config=%SUFTA%\config.txt
+
+%SUFTA_exe% %SUFTA_config%
 Start.exe
